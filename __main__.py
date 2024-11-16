9673e4cc8c81bad0f5e5856bbb3269c3a0448d75
Alexrubiks <alexrubiks@gmail.com>
2024-11-16 23:35:30 +0100

    premiere ligne de bus

--- __main__.py
+++ __main__.py
@@ -17,19 +17,27 @@ def main():
     clock = pygame.time.Clock()
 
     # Initialisation de la grille
-    
     render = Render(screen, ROWS, COLS, CELL_SIZE)
 
+    # Initialisation des arrêts de bus
+    bus_stops = [(100, 30), (430, 100), (530, 260), (530, 500), (260, 530)]
+    bus_path = [(100, 30), (430, 30), (430, 230), (530, 230), (530, 530), (260, 530)]
+    # bus_lines = BusLine(path, stops)
+    
     running = True
     while running:
         # Gestion des événements
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
         
         # Affichage des éléments à l'écran
         screen.fill((255, 255, 255))
         render.draw_grid(30, 30)
+        render.draw_bus_path(bus_path)
+        render.draw_bus_stops(bus_stops)
+
 
         # Actualisation de l'affichage
         pygame.display.flip()
--- src/render.py
+++ src/render.py
@@ -24,12 +24,49 @@ class Render:
 
         # Dessiner les lignes horizontales
         for row in range(self.rows + 1):
-            start_pos = (x, y + row * self.cell_size)
-            end_pos = (x + self.cols * self.cell_size, y + row * self.cell_size)
-            pygame.draw.line(self.screen, (200, 200, 200), start_pos, end_pos)
+            start_pos = (x-7, y + row * self.cell_size)
+            end_pos = (x+8 + self.cols * self.cell_size, y + row * self.cell_size)
+            pygame.draw.line(self.screen, (200, 200, 200), start_pos, end_pos, 16)
 
         # Dessiner les lignes verticales
         for col in range(self.cols + 1):
             start_pos = (x + col * self.cell_size, y)
             end_pos = (x + col * self.cell_size, y + self.rows * self.cell_size)
-            pygame.draw.line(self.screen, (200, 200, 200), start_pos, end_pos)
+            pygame.draw.line(self.screen, (200, 200, 200), start_pos, end_pos, 16)
+
+
+    def draw_bus_stops(self, bus_stops) -> None:
+        """
+        Dessine les arrêts de bus sur la grille.
+        :param bus_stops: Liste des coordonnées des arrêts de bus
+        """
+        for x, y in bus_stops:
+            pygame.draw.rect(
+                self.screen,
+                (255, 0, 0),
+                (x-11, y-11, 24, 24)
+            )
+            pygame.draw.rect(
+                self.screen,
+                (255, 255, 255),
+                (x-6, y-6, 14, 14)
+            )
+    
+    def draw_bus_path(self, bus_path) -> None:
+        """
+        Dessine le chemin du bus sur la grille.
+        :param bus_stops: Liste des coordonnées de la ligne de bus
+        """
+
+        for i in range(len(bus_path) - 1):
+            start_pos = (bus_path[i][0], bus_path[i][1])
+            end_pos = (bus_path[i+1][0], bus_path[i+1][1])
+            pygame.draw.line(self.screen, (255, 0, 0), start_pos, end_pos, 10)
+        
+        for x, y in bus_path:
+            pygame.draw.rect(
+                self.screen,
+                (255, 0, 0),
+                (x-4, y-4, 10, 10)
+            )
+
